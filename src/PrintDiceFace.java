9dbdd5f728ee8fe287f96d400bf84058eb5e4cfa
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-08 15:37:59 +0000

    initial init

---
+++ src/PrintDiceFace.java
@@ -0,0 +1,33 @@
+/**
+ * todo JavaDoc class
+ */
+public class PrintDiceFace {
+    /**
+     * todo JavaDoc main
+     * @param args
+     */
+    public static void main(String[] args) {
+
+    }
+}
+
+/* todo 1 Open the TODO tab to help you see what needs doing.
+            You can rename these comments to remove them from the list.
+            This is better than deleting them because you can see what did need
+            doing. Alternatively add "DONE" to the comment.
+*/
+// todo 2 Make multiple designs on paper
+// todo 3 Compare designs
+/* todo 4 Upload design(s) as images into the design folder,
+            keeping file size small
+*/
+// todo 5 Code, test, and refine a design
+// todo 6 Add comments and JavaDoc comments
+/* todo 7 Generate the JavaDocs from Tools > Generate JavaDoc...
+            Save in the javadoc directory
+            Open javadoc/index.html in IntelliJ and use the in-built browser
+            to view (the IJ logo that is left-most in the list of browsers)
+*/
+// todo 8 Optionally, start filling in writing.md
+// todo 9 Maybe code, test, and refine a different design for comparison
+// todo 10 Complete the reflections.md file
