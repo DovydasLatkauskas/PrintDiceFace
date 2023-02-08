607a9149952b24d8fb50d839f3ad4b7e3d7678ae
DovydasLatkauskas <dovydas.latkauskas@gmail.com>
2023-02-08 16:09:50 +0000

    functionality for checkInput added

--- src/helperClasses/checkInput.java
+++ src/helperClasses/checkInput.java
@@ -1,4 +1,15 @@
 package helperClasses;
 
 public class checkInput {
+    public static int checkInput(String argument){
+        switch(argument){
+            case "1": return 1;
+            case "2": return 2;
+            case "3": return 3;
+            case "4": return 4;
+            case "5": return 5;
+            case "6": return 6;
+            default: return 0; // if invalid input returns 0
+        }
+    }
 }
